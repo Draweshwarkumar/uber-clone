49ee7a66ac87d9641a028aec445234e69257154e
Draweshwarkumar <shivamjha22948@gmail.com>
2025-01-10 17:25:23 +0530

    creating login url

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -5,14 +5,14 @@ const {validationResult} = require('express-validator');
 
 module.exports.registerUser = async (req, res, next) =>{
     
-    const errors = validataionResult(req);
+    const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
 
     const {fullname, email, password} = req.body;
 
-    const hashedPassword = await userModel.hashedPassword(password);
+    const hashedPassword = await userModel.hashPassword(password);
 
     const user = await userService.createUser({
         firstname: fullname.firstname,
@@ -24,4 +24,30 @@ module.exports.registerUser = async (req, res, next) =>{
     const token = user.generateAuthToken();
     res.status(201).json({token,user});
 
+}
+
+module.exports.loginUser = async (req, res, next) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() });
+
+    }
+
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({email}).select('+password');
+
+    if(!user){
+        return res.status(401).json({message: 'Invalid email or password'});
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if(!isMatch){
+        return res.status(401).json({message: "Invalid email or password"});
+    }
+
+    const token = user.generateAuthToken();
+
+    res.status(200).json({token, user});
 }
