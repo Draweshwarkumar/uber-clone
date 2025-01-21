3a79db5b8c104bd5ec8542db93ad5e332e7d647b
Draweshwarkumar <shivamjha22948@gmail.com>
2025-01-21 13:06:43 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+const App = () => {
+    return (
+
+        <div>
+            <Routes>
+                <Route path='/' element={}/>
+            </Routes>
+            </div>
+
+    )
+}
+
+export default App
